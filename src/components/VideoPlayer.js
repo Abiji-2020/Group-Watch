fcfe2a676ad45404eed581cc8705247085a241b3
Abinand P <abinand0911@gmail.com>
2023-12-20 13:18:09 +0000

    new codebase

--- src/components/VideoPlayer.js
+++
@@ -1,9 +0,0 @@
-// VideoPlayer.js
-import React from 'react';
-import ReactPlayer from 'react-player';
-
-function VideoPlayer({ videoUrl }) {
-  return <ReactPlayer url={videoUrl} controls={true} width="100%" />;
-}
-
-export default VideoPlayer;

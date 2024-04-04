d218485545d0d592fab4fc8f2407dcdefbb496d2
Abinand P <abinand0911@gmail.com>
2024-04-04 08:06:51 +0000

    created dashboard and settins directory

--- auth/app/page.tsx
+++ auth/app/page.tsx
@@ -1,9 +1,9 @@
 
-
+import {Button} from "@/components/ui/button"
 export default function Home() {
   return (
-    <p className="font-semibold text-green-500"> 
-    Hello Auth!
-    </p>
+    <Button size="lg" > 
+      Click Me
+    </Button>
     );
 }
---
+++ auth/app/settings/page.tsx
@@ -0,0 +1,8 @@
+const settingsPage = () => {
+    return (
+        <div>
+        <h1>Settings Page</h1>
+        </div>
+    );
+}
+export default settingsPage;

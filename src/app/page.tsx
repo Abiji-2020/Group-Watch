8bd61964aa262ee1819bdc8a4141130d0a557bc0
Abinand P <abinand0911@gmail.com>
2024-04-05 17:10:40 +0000

    added theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-   <div>Hello World</div>
+   <div>Hello World
+
+    <Button>Click Me</Button>
+   </div>
   );
 }

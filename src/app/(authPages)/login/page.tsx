2769867b6a8247cf29ea0e034f6ad380bcc79836
Abinand P <abinand0911@gmail.com>
2024-04-06 21:26:34 +0530

    improving login page

--- src/app/(authPages)/login/page.tsx
+++ src/app/(authPages)/login/page.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 export default function Login() {
+  const [authState, setAuthStata] = useState<AuthStateType>({
+    email: "",
+    password: "",
+  });
+
+  const submit = () => {
+    console.log("The auth state is ", authState);
+  };
+
   return (
     <div className="bg-background">
       <div className="h-screen w-screen flex justify-center items-center">
@@ -14,29 +25,45 @@ export default function Login() {
           </div>
           <h1 className="text-2xl font-bold"> Login</h1>
           <p> Welcome Back</p>
+          <form onSubmit={submit}>
+            <div className="mt-5">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                onChange={(e) =>
+                  setAuthStata({ ...authState, email: e.target.value })
+                }
+              />
+            </div>
 
-          <div className="mt-5">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" placeholder="Enter your email" />
-          </div>
-
-          <div className="mt-5">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-            />
-          </div>
-          <div className="mt-5">
-            <button className="w-full bg-primary text-primary-foreground p-3 rounded-lg">
-              Login
-            </button>
-          </div>
+            <div className="mt-5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                onChange={(e) =>
+                  setAuthStata({ ...authState, password: e.target.value })
+                }
+              />
+            </div>
+            <div className="mt-5">
+              <button
+                className="w-full bg-primary text-primary-foreground p-3 rounded-lg"
+              >
+                Login
+              </button>
+            </div>
+          </form>
           <div className="mt-5">
             <span> Don't have an account?</span>
-            <Link href='/register' className="text-orange-400 font-bold"> Register</Link>
-            </div>
+            <Link href="/register" className="text-orange-400 font-bold">
+              {" "}
+              Register
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/app/(authPages)/register/page.tsx
+++ src/app/(authPages)/register/page.tsx
@@ -4,7 +4,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="bg-background">
       <div className="h-screen w-screen flex justify-center items-center">
@@ -12,12 +12,12 @@ export default function Login() {
           <div className="flex justify-center ">
             <Image src="/Images/logo.svg" width={50} height={50} alt="Logo" />
           </div>
-          <h1 className="text-2xl font-bold"> Login</h1>
-          <p> Welcome Back</p>
+          <h1 className="text-2xl font-bold"> Register</h1>
+          <p> Welcome To Threads</p>
 
           <div className="mt-5">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email" placeholder="Enter your email" />
+            <Label htmlFor="name">Name</Label>
+            <Input type="text" id="name" placeholder="Enter your Name" />
           </div>
 
           <div className="mt-5">

5017fa3c00d3e89acf588637b36d76fabc0a6a49
Abinand P <abinand0911@gmail.com>
2024-04-06 05:47:45 +0000

    Added some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-
+import ModeToggle from "@/components/common/ThemeToggleBtn";
 
 export default function Home() {
   return (
    <div>Hello World
 
     <Button>Click Me</Button>
+    <ModeToggle />
    </div>
   );
 }

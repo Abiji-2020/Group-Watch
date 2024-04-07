f7f3f50319d9fff20c657b6c796887ffea74c23d
Abinand P <abinand0911@gmail.com>
2024-04-07 09:27:15 +0530

    Checks added for unique email and username in register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import vine, { errors } from "@vinejs/vine";
 import { CustomErrorReporter } from "@/validation/CustomErrorReporter";
+import prisma from "@/DB/db.config";
 
 export async function POST(request: NextRequest) {
   try {
@@ -10,6 +11,34 @@ export async function POST(request: NextRequest) {
     vine.errorReporter = () => new CustomErrorReporter();
     const validator = vine.compile(registerSchema);
     const payload = await validator.validate(data);
+
+
+    // * To check if the email is already registered
+
+    const isEmailExist = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+    if (isEmailExist) {
+      return NextResponse.json({ status: 400, errors:{
+        email: "Already User exits with this Email."
+      } })
+    }
+
+    // To check Username
+
+    const isUsernameExist = await prisma.user.findUnique({
+      where: {
+        username: payload.username,
+      },
+    });
+
+    if (isUsernameExist){
+      return NextResponse.json({ status: 400, errors:{
+        username: "Username already taken."
+      } })
+    }
 
     // * To hash the password
 

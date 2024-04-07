ad907efe4c1c3e7d7740cc8090704d293690bb1b
Abinand P <abinand0911@gmail.com>
2024-04-07 17:54:43 +0530

    fixing bugs

--- src/app/(authPages)/login/page.tsx
+++ src/app/(authPages)/login/page.tsx
@@ -7,11 +7,14 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import axios from "axios";
-import {signIn} from 'next-auth/react'
+import { signIn } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 export default function Login() {
-
-
   const params = useSearchParams();
+  const router = useRouter();
+  const { status } = useSession();
 
   const [authState, setAuthStata] = useState<AuthStateType>({
     email: "",
@@ -19,25 +22,34 @@ export default function Login() {
   });
   const [errors, setErrors] = useState<AuthErrorType>({});
   const [loading, setLoading] = useState<boolean>(false);
-  const submit = (event : React.FormEvent) => {
-    axios.post('/api/auth/login', authState).then((res) => {
-     setLoading(false);
-     const response = res.data;
-     if(response.status === 200){
-       signIn("credentials", {
-          email: authState.email,
-          password: authState.password,
-          callbackUrl: "/",
-          redirect: true
-        
-       })
-      }else if(response.status === 400){
-        setErrors(response.errors);
-      }
-    }).catch((err) => {
-      setLoading(false);
-      console.log("The error is ", err);
-    });
+
+
+  useEffect(()=>{
+    if (status === "authenticated") {
+    router.push("/");
+    }
+  },[status])
+  const submit = (event: React.FormEvent) => {
+    axios
+      .post("/api/auth/login", authState)
+      .then((res) => {
+        setLoading(false);
+        const response = res.data;
+        if (response.status === 200) {
+          signIn("credentials", {
+            email: authState.email,
+            password: authState.password,
+            callbackUrl: "/",
+            redirect: true,
+          });
+        } else if (response.status === 400) {
+          setErrors(response.errors);
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log("The error is ", err);
+      });
     event.preventDefault();
   };
 
@@ -49,9 +61,14 @@ export default function Login() {
             <Image src="/Images/logo.svg" width={50} height={50} alt="Logo" />
           </div>
 
-          {params.get("message")?(<div className="bg-green-500 p-3 round-lg my-4"> 
-            <strong>Success!</strong>{params.get("message")}
-            </div>): (<></>)}
+          {params.get("message") ? (
+            <div className="bg-green-500 p-3 round-lg my-4">
+              <strong>Success!</strong>
+              {params.get("message")}
+            </div>
+          ) : (
+            <></>
+          )}
 
           <h1 className="text-2xl font-bold"> Login</h1>
           <p> Welcome Back</p>
@@ -84,7 +101,7 @@ export default function Login() {
             <div className="mt-5">
               <button
                 className="w-full bg-primary text-primary-foreground p-3 rounded-lg"
-                disabled = {loading}
+                disabled={loading}
               >
                 {loading ? "Processing..." : "Login"}
               </button>

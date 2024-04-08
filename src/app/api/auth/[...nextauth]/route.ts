ff52027047671202eeed963f59d61afa0831a5d8
Abinand P <abinand0911@gmail.com>
2024-04-08 14:15:04 +0000

    minor fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
-import {authOptions} from './options'
-import NextAuth from 'next-auth/next'
-const nextAuth = NextAuth(authOptions)
-export {nextAuth as GET, nextAuth as POST};
+import NextAuth from "next-auth/next";
+import { authOptions } from "./options";
+
+const nextAuth = NextAuth(authOptions);
+
+export { nextAuth as GET, nextAuth as POST };
